069162d02e5952c56a9cc3cf16bac0b32149c7a7
delia <delia.b.holman@gmail.com>
2024-10-29 11:51:22 -0500

    permutation file created

---
+++ permutation.py
@@ -0,0 +1,11 @@
+#finds all permutations of natural numbers
+
+#check for hamiltonian from nodes 1 until n-1 (not start or end node)
+
+#returns sequence of nodes if hamiltonian path
+#returns None if no paths
+
+#write unit tests
+
+#bonus: indicate which hamiltonian cycles are optimal in terms of overall distance
+#bonus 2: indicate the largest "clique" aka a subset of nodes representing a complete graph
