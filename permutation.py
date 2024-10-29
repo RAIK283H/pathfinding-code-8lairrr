adb692069a1b6acc438c3c4a6d52a38abd6842a5
delia <delia.b.holman@gmail.com>
2024-10-29 12:42:28 -0500

    update

--- permutation.py
+++ permutation.py
@@ -1,5 +1,17 @@
 #finds all permutations of natural numbers
 
+#Initialize the first permutation with <1 <2 ... <n
+firstPermutation = []
+mobileInteger = firstPermutation[len(firstPermutation)-1]
+#while there exists a mobile integer
+while (mobileInteger):
+    for i in range (len(firstPermutation) - 1): #  find the largest mobile integer k
+        if (firstPermutation[i] < firstPermutation[i + 1]) and (mobileInteger < firstPermutation[i + 1]):
+            mobileInteger = firstPermutation[i + 1]
+
+    #  swap k and the adjacent integer it is looking at
+    #  reverse the direction of all integers larger than k
+
 #check for hamiltonian from nodes 1 until n-1 (not start or end node)
 
 #returns sequence of nodes if hamiltonian path
