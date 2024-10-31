9e908eb61abe5409ce53d9db25c9581701e9a5cf
delia <delia.b.holman@gmail.com>
2024-10-31 15:36:13 -0500

    ok yeah we starting over

--- hamiltonianMain.py
+++ hamiltonianMain.py
@@ -1,23 +1,15 @@
 import global_game_data
 import graph_data
-from permutation import find_permutations, hamiltonian_cycle
+import permutation
 
 def main():
     graphIndex = global_game_data.current_graph_index
     graphStuff = graph_data.graph_data
     currentGraph = graphStuff[graphIndex]
 
-    all_cycles = find_permutations(currentGraph)  # finding all permutations
-
-    hamiltonian_graph = graph_data.hamiltonian_graph
-    valid_hamiltonian_cycles = hamiltonian_cycle(hamiltonian_graph)
-
-    if valid_hamiltonian_cycles == -1 or not valid_hamiltonian_cycles:
-        print(f"Hamiltonian graph: no valid Hamiltonian cycles found!")
-    else:
-        print(f"Hamiltonian graph: valid Hamiltonian cycles:")
-        for cycle in valid_hamiltonian_cycles:
-            print(cycle)
+    #prints if there is a hamiltonian graph in the permutations
+    #bonus: print optimal graph
+    #bonus 2: print largest set of nodes that is a complete graph
 
 if __name__ == "__main__":
     main()
--- permutation.py
+++ permutation.py
@@ -1,64 +1,28 @@
 #finds all permutations of natural numbers
 
-def generate_permutations(arr):
-    if len(arr) == 0:
-        return [[]]
-    result = []
-    for i in range(len(arr)):
-        current = arr[i]
-        remaining = arr[:i] + arr[i + 1:]
-        for perm in generate_permutations(remaining):
-            result.append([current] + perm)
-    return result
-
 def find_permutations(graph):
-    internal_nodes = range(1, len(graph) - 1)
-    all_cycles = []
+    #basically performing SJT and appending to an array
+    permutations = []
 
-    internal_nodes_list = list(internal_nodes)
-    for perm in generate_permutations(internal_nodes_list):
-        cycle = [0] + perm + [len(graph) - 1]
-        all_cycles.append(cycle)
+    return permutations
 
-    return all_cycles
+def validate_hamiltonian_cycle(permutations): #validates that each index in the array of permutations is hamiltonian
+    #check that each sequential number is in the number before adjacency list
+    # (including that the start node is in the end node's)
+    return -1
 
-
-def is_valid_move(node, path, graph):
-    if node not in graph[path[-1]]:
-        return False  #not connected to the previous node in path
-    if node in path:
-        return False  #node is already visited
-    return True
-
-def hamiltonian_cycle_backtracking(graph, path):
-    if len(path) == len(graph) - 1:
-        if path[0] in graph[path[-1]]:
-            return [path + [path[0]]]
-        return []
-
-    cycles = []
-    for node in range(1, len(graph) - 1):
-        if is_valid_move(node, path, graph):
-            path.append(node)
-            cycles.extend(hamiltonian_cycle_backtracking(graph, path))
-            path.pop()
-
-    return cycles
-
-def hamiltonian_cycle(graph):
-    all_hamiltonian_cycles = []
-    internal_nodes = range(1, len(graph) - 1)
-
-    for start_node in internal_nodes:
-        path = [start_node]
-        all_hamiltonian_cycles.extend(hamiltonian_cycle_backtracking(graph, path))
-
-    return all_hamiltonian_cycles if all_hamiltonian_cycles else -1
 
 #bonus: indicate which hamiltonian cycles are optimal in terms of overall distance
-#def optimal_cycle(): #what is the param??? do I need to loop each graph or is it finding the shortest path?
-    #return -1
+#def optimal_cycle(permutations):
+    #use hamiltonian_cycle on permutations
+    #cycle_weights = []
 
-#bonus 2: indicate the largest "clique" aka a subset of nodes representing a complete graph
+    #use xy coords of each thing -> graph_data[a][b][0] = x-y coordinates as tuple of point b in graph a
+
+    #return the lowest num and its corresponding permutation (same array in both arrays)
+
+#bonus 2: indicate the largest "clique" aka a subset of nodes representing a complete graph (all nodes are connected to each other)
+#use career fair lab !
 #def largest_clique():
-    #return -1
+    #generates subsets instead of permutations
+    #return largest set of nodes that is a complete graph
