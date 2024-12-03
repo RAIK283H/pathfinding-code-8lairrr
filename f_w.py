4f8bacfef4f0962f05d11cd6fee3bf8b11d8420f
delia <delia.b.holman@gmail.com>
2024-12-03 12:41:35 -0600

    updates

--- f_w.py
+++ f_w.py
@@ -1,8 +1,6 @@
 import graph_data
 import global_game_data
 
-graph = graph_data.graph_data[global_game_data.current_graph_index]
-
 #transforms graph data to n * n matrix for input
 def graph_data_to_graph_matrix_matrix(graph):
     n = len(graph) #number of nodes
@@ -10,8 +8,11 @@ def graph_data_to_graph_matrix_matrix(graph):
 
     for i, (_, adjacency_list) in enumerate(graph):
         graph_matrix[i][i] = 0 #distance from a node to itself is 0
-        for neighbor, weight in adjacency_list:
-            graph_matrix[i][neighbor] = weight #update with the given edge weight
+        for neighbor in adjacency_list:
+            if isinstance(neighbor, tuple): #if it has a weight
+                graph_matrix[i][neighbor[0]] = neighbor[1]
+            else: #there is no weight
+                graph_matrix[i][neighbor] = 1
 
     return graph_matrix
 
@@ -30,16 +31,16 @@ def generate_parent_matrix(graph_matrix):
     return parent_matrix
 
 #performs floyd warshall algorithm and returns all-pairs shortest path
-def floyd_warshall():
-    graph_matrix = graph_data_to_graph_matrix_matrix() #graph_matrix is a weighted graph matrix
+def floyd_warshall(graph):
+    graph_matrix = graph_data_to_graph_matrix_matrix(graph) #graph_matrix is a weighted graph matrix
     V = len(graph_matrix) #V is the number of nodes aka the height/weight of the graph_matrix matrix
     parent_matrix = generate_parent_matrix(graph_matrix) #parent_matrix is the matrix that holds parents of the graph_matrix matrix
 
     for k in range(V - 1):
         for i in range(V - 1):
             for j in range(V - 1):
-                if graph_matrix[i,k] + graph_matrix[k,j] < graph_matrix[i,j]:
-                    graph_matrix[i,j] = graph_matrix[i,k] + graph_matrix[k,j] #children matrix
+                if graph_matrix[i][k] + graph_matrix[k][j] < graph_matrix[i][j]:
+                    graph_matrix[i][j] = graph_matrix[i][k] + graph_matrix[k][j] #children matrix
                     parent_matrix[i][j] = parent_matrix[k][j] #parent matrix is updated for new path
 
     return graph_matrix, parent_matrix #return updated matrices
@@ -56,14 +57,53 @@ def floyd_warshall_paths(graph_matrix, parent_matrix):
             else: #reconstructing the path
                 path = []
                 current = i
-                while current != j:
-                    if current == -1: #if there's no valid path
+                visited = set()
+                while current is not None and current not in visited:
+                    visited.add(current)
+                    path.insert(0, current) #prepend node
+                    current = parent_matrix[i][current]
+                    if current is not None:  #if a cycle is detected return an empty path
                         path = []
-                        break
-                    path.append(current)
-                    current = parent_matrix[current][j]
-                if current != -1: #add the last node if path reconstruction was successful
-                    path.append(j)
-                all_paths.append((i, j, path)) #add the path for (i, j)
+                    all_paths.append((i, j, path)) #add the path for (i, j)
 
     return all_paths
+
+def main():
+    current_graph = graph_data.graph_data[global_game_data.current_graph_index]
+
+    #makes weight matrix
+    graph_matrix = graph_data_to_graph_matrix_matrix(current_graph)
+    print("weight matrix:")
+    for row in graph_matrix:
+        print(row)
+
+    #makes parent matrix
+    parent_matrix = generate_parent_matrix(graph_matrix)
+    print("\nparent matrix:")
+    for row in parent_matrix:
+        print(row)
+
+    print("\nperforming floyd-warshall")
+
+    #floyd-warshall
+    updated_graph_matrix, updated_parent_matrix = floyd_warshall(current_graph)
+
+    print("\nnew weight matrix:")
+    for row in updated_graph_matrix:
+        print(row)
+
+    print("\nnew parent matrix:")
+    for row in updated_parent_matrix:
+        print(row)
+
+    #finding paths
+    all_paths = floyd_warshall_paths(updated_graph_matrix, updated_parent_matrix)
+    print("\nall shortest paths:")
+    for start, end, path in all_paths:
+        if path:
+            print(f"shortest path from {start} to {end}: {path}")
+        else:
+            print(f"no path exists from {start} to {end}.")
+
+if __name__ == "__main__":
+    main()
